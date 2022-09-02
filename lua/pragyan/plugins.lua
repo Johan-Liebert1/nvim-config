304f558123dc790bd8488ad26e97a0d587e828af
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2022-09-02 23:13:01 +0530

    put back null ls

--- lua/pragyan/plugins.lua
+++ lua/pragyan/plugins.lua
@@ -105,7 +105,7 @@ return packer.startup(function(use)
     -- LSP
     use("neovim/nvim-lspconfig") -- enable LSP
     use("williamboman/nvim-lsp-installer") -- simple to use language server installer
-    --    use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
+    use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
 
     -- Telescope
     use({ "nvim-telescope/telescope.nvim", commit = "d96eaa914aab6cfc4adccb34af421bdd496468b0" })
