925b0eedc220a2dd189ef27acadeb4cb709b35c8
pragyan <pragyanpoudyal41999@gmail.com>
2023-04-10 15:31:01 +0530

    fix lsp hightlight override

--- init.lua
+++ init.lua
@@ -7,3 +7,8 @@ require("pragyan.lsp")
 require("pragyan.keymaps")
 
 vim.cmd("colorscheme materialOcean")
+
+-- Hide all semantic highlights
+for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+  vim.api.nvim_set_hl(0, group, {})
+end
