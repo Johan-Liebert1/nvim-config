75d99c7cff7f077b6a515ae5ceed6ae77e67272b
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2024-02-15 19:33:50 +0530

    Disable mouse cause it's poop

--- lua/pragyan/options.lua
+++ lua/pragyan/options.lua
@@ -33,6 +33,7 @@ vim.opt.scrolloff = 8 -- is one of my fav
 vim.opt.sidescrolloff = 8
 vim.opt.guifont = "monospace:h17" -- the font used in graphical neovim applications
 vim.opt.syntax = "off"
+vim.opt.mouse = ""
 
 vim.opt.shortmess:append("c")
 
