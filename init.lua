341d290d87ede8d8c012b430be919c589f35576f
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2024-04-10 21:04:05 +0530

    NvimTree multiple delete

--- init.lua
+++ init.lua
@@ -19,8 +19,12 @@ vim.api.nvim_set_hl(0, '@lsp.type.function', {
     fg = '#6695fa'
 })
 
--- lang.lua - Neovim syntax highlighting for .berk files
+-- lang.lua - Neovim syntax highlighting for .cberk files
 vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
     pattern = "*.cberk",
     command = "set filetype=lua",
 })
+
+PrettyPrint = function (table)
+    vim.print(table)
+end
--- lua/pragyan/plug-configs/nvimTree.lua
+++ lua/pragyan/plug-configs/nvimTree.lua
@@ -1,9 +1,54 @@
 -- examples for your init.lua
 
 -- OR setup with some options
-require("nvim-tree").setup({
+local nvim_tree = require("nvim-tree")
+
+nvim_tree.setup({
+    on_attach = function (bufnr)
+        local api = require("nvim-tree.api")
+
+        local opts = function(desc)
+            return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+
+        -- mark operation
+        local mark_move_j = function()
+            api.marks.toggle()
+            vim.cmd("norm j")
+        end
+
+        local mark_move_k = function()
+            api.marks.toggle()
+            vim.cmd("norm k")
+        end
+
+        local mark_remove = function()
+            local marks = api.marks.list()
+
+            if #marks == 0 then
+                table.insert(marks, api.tree.get_node_under_cursor())
+            end
+
+            vim.ui.input({ prompt = string.format("Remove/Delete %s files? [y/n] ", #marks) }, function(input)
+                if input == "y" then
+                    for _, node in ipairs(marks) do
+                        api.fs.remove(node)
+                    end
+
+                    api.marks.clear()
+                    api.tree.reload()
+                end
+            end)
+        end
+
+        vim.keymap.set("n", "<leader>df", mark_remove, opts("Remove File(s)"))
+        vim.keymap.set("n", "J", mark_move_j, opts("Toggle Bookmark Down"))
+        vim.keymap.set("n", "K", mark_move_k, opts("Toggle Bookmark Up"))
+    end,
+
     -- Changes the tree root directory on `DirChanged` and refreshes the tree.
     sync_root_with_cwd = true,
+
     view = {
         adaptive_size = false,
         mappings = {
