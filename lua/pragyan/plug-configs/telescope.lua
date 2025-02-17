e3f6e6c65b1170ee4354fa42f1c4fe06d70f2537
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2025-02-17 16:43:41 +0530

    Telescope ignore `target` folder
    Signed-off-by: Pragyan Poudyal <pragyanpoudyal41999@gmail.com>

--- lua/pragyan/plug-configs/telescope.lua
+++ lua/pragyan/plug-configs/telescope.lua
@@ -12,7 +12,7 @@ telescope.setup({
         prompt_prefix = " ",
         selection_caret = " ",
         path_display = { "truncate" },
-        file_ignore_patterns = { "^node_modules/", ".git/" },
+        file_ignore_patterns = { "^node_modules/", ".git/", "target" },
 
         mappings = {
             i = {
