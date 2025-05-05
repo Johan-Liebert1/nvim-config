eb6fe77d40def1cf317a40a19514ca339e40ac09
Pragyan Poudyal <pragyanpoudyal41999@gmail.com>
2025-05-05 19:46:53 +0530

    Add render markdown plugin

---
+++ lua/pragyan/plug-configs/devicons.lua
@@ -0,0 +1,10 @@
+require'nvim-web-devicons'.setup {
+    override = {
+        rs = {
+            icon = "",
+            color = "#dea584",
+            cterm_color = "216",
+            name = "Rs"
+        }
+    }
+};
--- lua/pragyan/plugins.lua
+++ lua/pragyan/plugins.lua
@@ -145,6 +145,20 @@ return packer.startup(function(use)
     -- to automatically add Neovim runtime and type annotations for lua_ls
     use 'folke/neodev.nvim'
 
+    use({
+        'MeanderingProgrammer/render-markdown.nvim',
+        after = { 'nvim-treesitter' },
+        -- requires = { 'echasnovski/mini.nvim', opt = true }, -- if you use the mini.nvim suite
+        -- requires = { 'echasnovski/mini.icons', opt = true }, -- if you use standalone mini plugins
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }, -- if you prefer nvim-web-devicons
+        config = function()
+            require('render-markdown').setup({
+                options = {
+                    render_modes = { 'n', 'c', 't' }
+                }
+            })
+        end,
+    })
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
